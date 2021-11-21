09b2335a40e5560da1658684e517ba4b52438086
sahil <sahil91098singh@gmail.com>
2021-11-21 13:22:47 +0530

    First spring app done.

---
+++ spring-demo-one/src/com/sahil/springdemo/FootballCoach.java
@@ -0,0 +1,12 @@
+package com.sahil.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Do a plank for 2 minutes.";
+	}
+	
+	
+
+}
---
+++ spring-demo-one/src/com/sahil/springdemo/HelloSpringApp.java
@@ -0,0 +1,27 @@
+package com.sahil.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+	
+	public static void main(String[] args) {
+		
+		// Load the spring configuration file
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Retrieve bean from Spring container
+		
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		// Call methods on the bean
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// Close the context
+		
+		context.close();
+		
+	}
+
+}
