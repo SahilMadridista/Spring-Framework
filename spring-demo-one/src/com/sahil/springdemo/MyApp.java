450896d23fc8379a37efc11de1ead282476d5047
sahil <sahil91098singh@gmail.com>
2021-11-21 12:15:42 +0530

    Project initialized

---
+++ spring-demo-one/src/com/sahil/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.sahil.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		// Object creation
+		Coach theCoach = new TrackCoach();
+		
+		// Using object
+		System.out.println(theCoach.getDailyWorkout());
+		
+
+	}
+
+}
---
+++ spring-demo-one/src/com/sahil/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.sahil.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run for 2 kilometers.";
+	}
+
+}
