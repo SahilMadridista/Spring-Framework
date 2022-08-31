97eaf5d6c1247f480b2c86c9f696290e91b68636
sahil <sahil91098singh@gmail.com>
2022-08-31 23:29:13 +0530

    Eager loading example using debugger.

---
+++ OneToManyDemo/src/main/java/com/sahil/onetomany/bi/EagerLazyDemo.java
@@ -0,0 +1,59 @@
+package com.sahil.onetomany.bi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.sahil.hibernate.entity.Student;
+import com.sahil.mapping.entity.Course;
+import com.sahil.mapping.entity.Instructor;
+import com.sahil.mapping.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		
+		try {
+			
+			// Start a transaction
+			session.beginTransaction();
+			
+			// Get the instructor from DB
+			
+			int id = 2;
+			
+			Instructor instructor = session.get(Instructor.class, id);
+			
+			System.out.println("Debug- Instructor is : " + instructor);
+			
+			// Get courses
+			
+			System.out.println("Courses : " + instructor.getCourses());
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done");
+			
+		}finally {
+			
+			session.close();
+			factory.close();
+			
+		}
+		
+
+	}
+
+}
