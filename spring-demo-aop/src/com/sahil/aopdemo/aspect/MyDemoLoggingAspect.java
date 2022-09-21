9a272003a0a046c0da29b2ca9562d5760c9a9d6f
sahil <sahil91098singh@gmail.com>
2022-09-21 12:46:07 +0530

    Reusable pointcut expression using @Pointcut annotation

--- spring-demo-aop/src/com/sahil/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/sahil/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.sahil.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,7 +16,10 @@ public class MyDemoLoggingAspect {
 	// @Before("execution(* add*(..))")
 	// @Before("execution(* com.sahil.aopdemo.dao.*.*(..))")
 	
-	@Before("execution(* com.sahil.aopdemo.dao.*.*(..))")
+	@Pointcut("execution(* com.sahil.aopdemo.dao.*.*(..))")
+	private void forDAOPackage() {}
+	
+	@Before("forDAOPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("------> Executing @Before advice on addAccount()");
 	}
