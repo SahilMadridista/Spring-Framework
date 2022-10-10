6040db38b1c9323ea01f7841f5e0060c82d6c267
sahil <sahil91098singh@gmail.com>
2022-10-10 15:52:54 +0530

    Spring security project initialized.

---
+++ spring-security-demo-02-base-security/src/main/java/com/sahil/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.sahil.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig 
+	extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// Add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("karim").password("1234").roles("ST"))
+			.withUser(users.username("luka").password("1234").roles("CM"))
+			.withUser(users.username("eder").password("1234").roles("CB"));
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ spring-security-demo-02-base-security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,15 @@
+<html>
+
+<head> Real Madrid home page </head>
+
+<body>
+
+<h2>Hala Madrid !! We have 14 Champions Leagues.</h2>
+
+<hr>
+
+Welcome to the best club in the world.
+
+</body>
+
+</html>
