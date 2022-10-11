6f3225cab054ab187f7c34c3a29fe168b794d53b
sahil <sahil91098singh@gmail.com>
2022-10-11 16:25:48 +0530

    Basic spring security custom login page with error message

---
+++ spring-security-demo-03-custom-login/src/main/java/com/sahil/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.sahil.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+	extends AbstractSecurityWebApplicationInitializer {
+
+	
+	
+	
+}
