fc4c7f0f22c819d41310320ac52ccda0b2bd76f2
sahil <sahil91098singh@gmail.com>
2022-10-12 12:23:54 +0530

    Spring security login using bootstrap form

---
+++ spring-security-demo-04-bootstrap-login/src/main/java/com/sahil/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.sahil.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginForm() {
+		// return "plain-login";
+		return "fancy-login";
+	}
+	
+}
