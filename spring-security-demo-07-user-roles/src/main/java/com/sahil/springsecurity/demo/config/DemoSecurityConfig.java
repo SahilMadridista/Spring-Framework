960316bc0077338859414524faa1626655e431e3
sahil <sahil91098singh@gmail.com>
2022-10-18 13:20:26 +0530

    Changes in security config for custom access denied page.

--- spring-security-demo-07-user-roles/src/main/java/com/sahil/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-07-user-roles/src/main/java/com/sahil/springsecurity/demo/config/DemoSecurityConfig.java
@@ -58,7 +58,10 @@ public class DemoSecurityConfig
 		.permitAll()
 		.and()
 		.logout()
-		.permitAll();
+		.permitAll()
+		.and()
+		.exceptionHandling()
+		.accessDeniedPage("/access-denied");
 		
 		
 	}
