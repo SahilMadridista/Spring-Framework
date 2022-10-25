e36cb21839ef744d283c52c76a14adb3070c19ee
sahil <sahil91098singh@gmail.com>
2022-10-25 10:04:13 +0530

    Refactored the code in StudentController.

--- spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
+++ spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
@@ -2,26 +2,38 @@ package com.sahil.springdemo.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.sahil.springdemo.entity.Student;
 
 @RestController
 @RequestMapping("/api")
 public class StudentController {
 
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	private List<Student> myList;
+	
+	// Define @PostConstruct to load the student data... only once
+	
+	@PostConstruct
+	public void loadData() {
 		
-		List<Student> myList = new ArrayList<Student>();
+		myList = new ArrayList<Student>();
 		
 		myList.add(new Student("Karim", "Benzema"));
 		myList.add(new Student("Luka", "Modric"));
 		myList.add(new Student("Federico", "valverde"));
 		myList.add(new Student("Toni", "Kroos"));
-		myList.add(new Student("Gareth", "Bale"));
+		myList.add(new Student("Dani","Carvajal"));
 		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		return myList;
 	}
 	
