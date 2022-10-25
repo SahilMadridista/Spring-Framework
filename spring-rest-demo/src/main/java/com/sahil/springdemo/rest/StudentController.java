0f8f17fb4d062d79a82f21068e05c568879a5719
sahil <sahil91098singh@gmail.com>
2022-10-25 10:28:00 +0530

    Using @PathVariable in StudentController to get single entry.

--- spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
+++ spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +37,18 @@ public class StudentController {
 	public List<Student> getStudents(){
 		return myList;
 	}
+	
+	
+	// Define endpoint for "/students/{studentId}"- Return student at index
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		// Just index into the list... keeping it simple for now
+		
+		return myList.get(studentId);
+		
+	}
+	
 	
 }
