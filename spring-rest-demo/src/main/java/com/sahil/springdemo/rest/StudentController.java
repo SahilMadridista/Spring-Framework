a3e7c76202d308180e446b6cb4f6e5f9a2f1915d
sahil <sahil91098singh@gmail.com>
2022-10-25 11:12:16 +0530

    Handled the exception using ErrorResponse class, Exception class and Exceptionhandler method.

--- spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
+++ spring-rest-demo/src/main/java/com/sahil/springdemo/rest/StudentController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,9 +49,47 @@ public class StudentController {
 		
 		// Just index into the list... keeping it simple for now
 		
+		// Check if the student id is valid
+		
+		if((studentId>=myList.size()) || studentId<0) {
+			throw new StudentNotFoundException("Student id not found: " + studentId);
+		}
+		
 		return myList.get(studentId);
 		
 	}
+	
+	// Add an exception handler using @ExceptionHandler
+	
+	// StudentErrorResponse- Type of response body
+	
+	// StudentNotFoundException- Exception type to handle
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+		
+		// Create a studentErrorResponse
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// Return responseEntity
+		
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
