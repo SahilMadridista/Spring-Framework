d07e31e813590b9c1f9fe792c0e60f3b2dcca244
sahil <sahil91098singh@gmail.com>
2022-10-31 13:31:06 +0530

    Added new end point in repository using @RepositoryRestResource

--- cruddemo-springdata-rest-demo/src/main/java/com/sahil/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo-springdata-rest-demo/src/main/java/com/sahil/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.sahil.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sahil.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// That's it. No need to write any code here
